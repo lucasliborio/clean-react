a0159909bb86c55a6d40a292139550e5a68fee81
lucasliborio <lucasl.troncoso@gmail.com>
2022-10-17 10:47:53 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
+    '<rootDir>/src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  
+    '.+\\.(ts|tsx)$': ['@swc/jest']
+  }
 }
