73d7b47599cc333655f588207966d32da314d190
lucasliborio <lucasl.troncoso@gmail.com>
2022-10-19 19:09:00 -0300

    feat: ensure: Axios HttpClient returns correc values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,6 +5,13 @@ import { HttpPostParams } from "@/data/protocols/http"
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const mockedAxiosResult = {
+  data: { name: "Lucas" },
+  status: faker.datatype.number()
+}
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
+
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
@@ -20,5 +27,14 @@ describe('AxiosHttpClient', () => {
     const sut = makeSut()
     await sut.post({ url, body })
     expect(mockedAxios.post).toHaveBeenCalledWith(url, body)
+  })
+  test('should call axios with correct URL and Body values', async () => {
+    const { url, body } = mockPostParams()
+    const sut = makeSut()
+    const httpResponse = await sut.post({ url, body })
+    expect(httpResponse).toEqual({
+      body: mockedAxiosResult.data,
+      statusCode: mockedAxiosResult.status
+    })
   })
 })
