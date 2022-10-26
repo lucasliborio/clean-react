1ac53d90e5fb6c332e46acb5d677b06415c790cf
lucasliborio <lucasl.troncoso@gmail.com>
2022-10-26 14:32:53 -0300

    test: ensure that Login will not render spinnerComponent and errorMessage on start

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react'
 import { LoginPage } from './login'
 
 describe('Login Component', () => {
-  test('', () => {
-    render(<LoginPage />)
+  test('should not render spinner and error message on start', () => {
+    const { getByTestId } = render(<LoginPage />)
+    const erroWrap = getByTestId('error-wrap')
+    expect(erroWrap.childElementCount).toBe(0)
   })
 })
