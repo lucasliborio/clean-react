a5be21c0c203b2a8a49bf99da6541fcf3bf372ff
lucasliborio <lucasl.troncoso@gmail.com>
2022-10-26 14:32:31 -0300

    feat: ensure that Login will not render spinnerComponent and errorMessage on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,14 @@
-import React from "react"
+import { FormContext } from "@/presentation/context/form/form-context"
+import React, { useContext } from "react"
 import { Spinner } from "../spinner/spinner"
 import Styles from './form-status-styles.scss'
 
 export const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(FormContext)
   return (
-    <div className={Styles.errorWrapper}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Error</span>
+    <div data-testid="error-wrap" className={Styles.errorWrapper}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && <span className={Styles.error}>Error</span>}
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,19 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import Style from './login-styles.scss'
 import { Header, Footer, CustomInput, FormStatus } from "@/presentation/components"
+import { FormContext } from "@/presentation/context/form/form-context"
 
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 export const LoginPage: React.FC = () => {
+  const [state, setState] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <section className={Style.login}>
       <Header />
-      <form className={Style.form}>
-        <h2>Login</h2>
-        <CustomInput type="email" name="email" placeholder="Enter your email" />
-        <CustomInput type="password" name="password" placeholder="Enter your password" />
-        <button type="submit">LogIn</button>
-        <span className={Style.link}>Sign In</span>
-        <FormStatus />
-      </form>
+      <FormContext.Provider value={state}>
+        <form className={Style.form}>
+          <h2>Login</h2>
+          <CustomInput type="email" name="email" placeholder="Enter your email" />
+          <CustomInput type="password" name="password" placeholder="Enter your password" />
+          <button type="submit">LogIn</button>
+          <span className={Style.link}>Sign In</span>
+          <FormStatus />
+        </form>
+      </FormContext.Provider>
       <Footer />
     </section >
   )
