122191789c6bc272b9f277ab917b42d02201a940
lucasliborio <lucasl.troncoso@gmail.com>
2023-01-03 10:47:40 -0300

    feat: change notebook

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Router } from "@/presentation/components/router/router"
-import { RouterProvider } from "react-router-dom"
-
 import "@/presentation/styles/globals.scss"
 ReactDOM.render(
   <Router/>,
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react"
-import Style from './login-styles.scss'
-import { Header, Footer, CustomInput, FormStatus } from "@/presentation/components"
-import { FormContext } from "@/presentation/context/form/form-context"
+import React, { useState } from "react";
+import Style from "./login-styles.scss";
+import {
+  Header,
+  Footer,
+  CustomInput,
+  FormStatus,
+} from "@/presentation/components";
+import { FormContext } from "@/presentation/context/form/form-context";
 
 type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
+  isLoading: boolean;
+  errorMessage: string;
+};
 export const LoginPage: React.FC = () => {
   const [state, setState] = useState<StateProps>({
     isLoading: false,
-    errorMessage: ''
-  })
+    errorMessage: "",
+  });
 
   return (
     <section className={Style.login}>
@@ -19,14 +24,24 @@ export const LoginPage: React.FC = () => {
       <FormContext.Provider value={state}>
         <form className={Style.form}>
           <h2>Login</h2>
-          <CustomInput type="email" name="email" placeholder="Enter your email" />
-          <CustomInput type="password" name="password" placeholder="Enter your password" />
-          <button data-testid="submit-button" disabled type="submit">LogIn</button>
+          <CustomInput
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+          />
+          <CustomInput
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+          <button data-testid="submit-button" disabled type="submit">
+            Log In
+          </button>
           <span className={Style.link}>Sign In</span>
           <FormStatus />
         </form>
       </FormContext.Provider>
       <Footer />
-    </section >
-  )
-}
+    </section>
+  );
+};
